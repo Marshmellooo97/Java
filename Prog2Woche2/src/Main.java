206ed7eb1372a797c90d4fbf60789a6270a45d4c
Justin Simon <justin.simon@informatik.hs-fulda.de>
2022-04-27 21:00:05 +0200

    B

---
+++ Prog2Woche2/src/Main.java
@@ -0,0 +1,21 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Automatisch generierter Methodenstub
+
+		int x = 0;
+		System.out.println(hat222(732));
+		
+		
+		
+	}
+	
+	public static int hat222(int x) {
+		int erg;
+		erg = x % 10;
+		
+		return erg;
+	}
+
+}
