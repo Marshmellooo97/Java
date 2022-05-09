56c9e4ed89c749544817967fc75b1016426f8c82
Marshmelooo97 <91906479+Marshmelooo97@users.noreply.github.com>
2022-05-09 21:13:01 +0200

    Woche4

--- Prog2Woche4/src/MainWoche4.java
+++ Prog2Woche4/src/MainWoche4.java
@@ -4,6 +4,25 @@ public class MainWoche4 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
+		
+		
+		
 	}
 
+	
+	class Frucht {
+		 private int weight = 100;
+		}
+
+		class Apfel extends Frucht {
+		 public static void main(String[] args) {
+		   Frucht f1 = new Frucht();
+		   Apfel a1 = new Apfel();
+		   f1 = a1;
+		 }
+		}
+	
+	
+	
 }
